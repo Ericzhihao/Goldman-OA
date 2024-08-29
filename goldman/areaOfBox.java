a9430c5e44919b21cc21ca1f65dceb87890bc97c
EricZhihao <zhihao.yu19@gmail.com>
2024-08-29 07:19:37 -0700

    update

--- goldman/areaOfBox.java
+++ goldman/areaOfBox.java
@@ -7,24 +7,23 @@ public class areaOfBox {
     public int[] numberOfWays(int[][] queries){
         int m = 0;
         int n = 0;
-        int min = 0;
-        int res = 0;
 
-        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
         for(int[] query : queries){
             m = query[0];
             n = query[1];
-            min = Math.min(m,n);
-            res = 0;
+            int res = 0;
+            int min = Math.min(m,n);
+
             for(int i = 1; i <= min; i++){
-                res += (n - i + 1) * (m - i + 1);
+                res += (m - i + 1) * (n - i + 1);
             }
             list.add(res);
         }
-        int[] result = new int[list.size()];
+        int[] res = new int[list.size()];
         for(int i = 0; i < list.size(); i++){
-            result[i] = list.get(i);
+            res[i] = list.get(i);
         }
-        return result;
+        return res;
     }
 }
