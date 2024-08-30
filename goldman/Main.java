a1c4ddc0d6bc10c172224b5f6927161a599205fc
EricZhihao <zhihao.yu19@gmail.com>
2024-08-30 00:21:01 -0700

    numberString

--- goldman/Main.java
+++ goldman/Main.java
@@ -36,9 +36,13 @@ public class Main {
 //        // Print the result
 //        System.out.println(t.checkPointsInTriangle(x1, y1, x2, y2, x3, y3, xp, yp, xq, yq));
 
-        BetterStringCompression bsc = new BetterStringCompression();
-        String input = "a10c20b30a5";
-        String compressedString = bsc.compressString(input);
-        System.out.println(compressedString);
+//        BetterStringCompression bsc = new BetterStringCompression();
+//        String input = "a10c20b30a5";
+//        String compressedString = bsc.compressString(input);
+//        System.out.println(compressedString);
+
+         numberStirng ns = new numberStirng();
+         String s = "2101";
+         System.out.println(ns.numDecodings(s));
     }
 }
--- goldman/numberStirng.java
+++ goldman/numberStirng.java
@@ -1,4 +1,34 @@
 package goldman;
 
 public class numberStirng {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int n = s.length();
+        int[] dp = new int[n + 1];
+
+        // Base cases
+        dp[0] = 1;  // An empty string has one way to decode
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;  // The first character can't be '0'
+
+        // Build the dp array
+        for (int i = 2; i <= n; i++) {
+            int oneDigit = Integer.parseInt(s.substring(i - 1, i));
+            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+
+            // If the one digit number is valid (1-9), it contributes dp[i-1]
+            if (oneDigit >= 1 && oneDigit <= 9) {
+                dp[i] += dp[i - 1];
+            }
+
+            // If the two digit number is valid (10-26), it contributes dp[i-2]
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
 }
